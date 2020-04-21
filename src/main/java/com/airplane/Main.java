707e8b080c09a3c06ac76edd6196f78fa917a853
priyanka thakker <thakker747@gmail.com>
2020-04-20 22:42:36 -0400

    initial commit

---
+++ src/main/java/com/airplane/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.airplane;
+import java.util.Scanner;
+
+
+public class Main {
+   public static void main(String args[])
+{
+   Airplane airplane = new Airplane();
+   System.out.println(airplane.toString());
+
+} 
+}
+
+
+
+
